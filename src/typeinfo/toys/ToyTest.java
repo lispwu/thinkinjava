a086d4d0ab0ba6d62ed3f5d3803d94c89061a51a
lispwu <626731411@qq.com>
2018-05-08 22:51:35 +0800

    practise

---
+++ src/typeinfo/toys/ToyTest.java
@@ -0,0 +1,48 @@
+package typeinfo.toys;
+
+interface HasBatteries{}
+interface Waterproof{}
+interface Shoots{}
+
+class Toy{
+    Toy(){}
+    Toy(int i){}
+}
+
+class FancyToy extends Toy
+implements HasBatteries,Waterproof,Shoots{
+    FancyToy(){super(1);}
+}
+
+public class ToyTest {
+    static void printInfo(Class cc){
+        System.out.println("Class name: " + cc.getName() +
+        " is interface? [" + cc.isInterface() + "]");
+        System.out.println("Simple Name: " + cc.getSimpleName());
+        System.out.println("Canonical Name " + cc.getCanonicalName());
+    }
+
+    public static void main(String[] args){
+        Class c = null;
+        try{
+            c = Class.forName("typeinfo.toys.FancyToy");
+        }catch (ClassNotFoundException e){
+            System.out.println("Can't find FancyToy");
+            System.exit(1);
+        }
+        printInfo(c);
+        for(Class face : c.getInterfaces())
+            printInfo(face);
+        Class up = c.getSuperclass();
+        Object obj = null;
+        try{
+            obj = up.newInstance();
+        }catch (InstantiationException e){
+            System.out.println("Cannot instantiate");
+            System.exit(1);
+        }catch (IllegalAccessException e){
+            System.out.println("Cannot access");
+        }
+        printInfo(obj.getClass());
+    }
+}
